038af481002f79d0502ed7ad203c631830560868
benfry128 <benfry128@gmail.com>
2025-04-12 17:42:50 -0400

    Add toolbar

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,19 +1,32 @@
 import React, {useEffect, useState} from 'react';
+import AppBar from '@mui/material/AppBar';
+import Button from '@mui/material/Button';
+import { Toolbar } from '@mui/material';
 
 function index() {
   useEffect(() => {
-    fetch('http://localhost:8080/tracks/200')
-      .then(
-      data => {
-        console.log(data.json());
-      }
-    );
+    // fetch('http://localhost:8080/tracks/200')
+    //   .then(
+    //   data => {
+    //     console.log(data.json());
+    //   }
+    // );
   }, []);
   
   return (
-    <div>
-      AMORG
-    </div>
+    <AppBar position='static'>
+      <Toolbar>
+        <Button>
+          Home
+        </Button>
+        <Button>
+          Request Albums
+        </Button>
+        <Button>
+          Amorogs
+        </Button>
+      </Toolbar>
+    </AppBar>
   )
 }
 
