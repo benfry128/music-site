e9523cb486d19fad2caa5a2bc2fe5e4a30ec79ac
benfry128 <benfry128@gmail.com>
2025-04-13 00:10:03 -0400

    Added albums page and updated index with stuff

--- client/pages/albums.tsx
+++ client/pages/albums.tsx
@@ -2,23 +2,14 @@ import React, { useEffect } from 'react';
 import { Typography, Box, Stack } from '@mui/material';
 
 function index() {
-  useEffect(() => {
-    // fetch('http://localhost:8080/tracks/200')
-    //   .then(
-    //   data => {
-    //     console.log(data.json());
-    //   }
-    // );
-  }, []);
-  
-  return (
+   return (
     <Box sx={{ width: '100%', display: 'flex', flexDirection: 'row', justifyContent: 'center'}}>
       <Stack direction="column" alignItems='center' spacing={3}>
         <Typography variant='h3'>
-          Welcome to my Wobsite
+          Albums
         </Typography>
         <Typography maxWidth='50%'>
-          This is the homepage. There is nothing here for now. If you came here from my Instagram account, you may be interested in the Request Albums page or the listening data.
+          Here you can request albums for me to try. 
         </Typography>
       </Stack>
     </Box>
