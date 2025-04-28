c5e3db56e5b2457f23dc855184787a45c68af64b
benfry128 <benfry128@gmail.com>
2025-04-27 20:18:10 -0400

    Add swiper styles and my own stylesheet

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,6 +1,10 @@
 import type { AppProps } from "next/app";
 import Layout from "@/components/Layout";
 
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import './styles.css';
+
 export default function App({ Component, pageProps }: AppProps) {
   return <Layout>
     <Component {...pageProps} />
