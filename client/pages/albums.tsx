e82d2f9a6bc9c8243ca889832dc903da54a1d04e
benfry128 <benfry128@gmail.com>
2025-04-27 20:18:55 -0400

    Add swipeable carousel to albums page with first 10 albums from db

--- client/pages/albums.tsx
+++ client/pages/albums.tsx
@@ -1,8 +1,16 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Typography, Box, Stack } from '@mui/material';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { EffectCoverflow } from 'swiper/modules';
+import Image from 'next/image';
 
 function index() {
-   return (
+  var numbers = []
+  for (var i = 1; i < 10; i++) {
+    numbers.push(i);
+  }
+  console.log(numbers);
+  return (
     <Box sx={{ width: '100%', display: 'flex', flexDirection: 'row', justifyContent: 'center'}}>
       <Stack direction="column" alignItems='center' spacing={3}>
         <Typography variant='h3'>
@@ -11,6 +19,34 @@ function index() {
         <Typography maxWidth='50%'>
           Here you can request albums for me to try. 
         </Typography>
+        <Swiper
+          effect={'coverflow'}
+          grabCursor={true}
+          centeredSlides={true}
+          slidesPerView={'auto'}
+          slideToClickedSlide={true}
+          coverflowEffect={{
+            rotate: 0,
+            scale: 1.25,
+            depth: 750,
+            modifier: 1,
+            slideShadows: false
+          }}  
+          modules={[EffectCoverflow]}
+        >
+          <>
+            {numbers.map((num) => 
+              <SwiperSlide>
+                <Image 
+                  src={`/${num}.jpg`} 
+                  width={640}
+                  height={640}
+                  alt='HI'
+                />
+              </SwiperSlide>
+            )}
+          </>
+        </Swiper>
       </Stack>
     </Box>
   );
