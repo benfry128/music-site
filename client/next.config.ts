75d9019b56d684dd208d969b8287faf1703fd18c
benfry128 <benfry128@gmail.com>
2025-05-08 00:43:07 -0400

    Updated next config to route api requests to flask app

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,6 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  rewrites: async () => {
+    return [
+      {
+        source: '/api/:path*',
+        destination:
+        process.env.NODE_ENV === 'development' ? 'http://127.0.0.1:5000/:path*' : '/api/',
+      }
+    ]
+  },
   /* config options here */
   reactStrictMode: true,
 };
