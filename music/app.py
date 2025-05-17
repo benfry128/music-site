6f1bcae8dfbd4cb00fc65693f3de73fe93519fba
benfry128 <benfry128@gmail.com>
2025-05-16 23:45:45 -0400

    Set up CORS to allow access to API from frontend

--- music/app.py
+++ music/app.py
@@ -2,21 +2,13 @@ from flask import Flask, jsonify, make_response
 
 import tracks
 import albums
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
-# app.register_blueprint(tracks.bp)
+app.register_blueprint(tracks.bp)
 app.register_blueprint(albums.bp)
-
-@app.route("/")
-def hello_from_root():
-    return jsonify(message='Hello from root!')
-
-
-@app.route("/hello")
-def hello():
-    return jsonify(message='Hello from path!')
-
 
 @app.errorhandler(404)
 def resource_not_found(e):
