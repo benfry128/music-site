1382c9c0ff299e2c5adc691dfece8662d9a79194
benfry128 <benfry128@gmail.com>
2025-05-20 21:56:01 -0400

    Turn off /tracks bp for now

--- music/tracks.py
+++ music/tracks.py
@@ -6,26 +6,28 @@ bp = Blueprint('tracks', __name__, url_prefix='/tracks')
 
 @bp.before_request
 def load_db():
-    MYSQL_PWD = os.getenv('MYSQL_PWD')
+    print("HI")
+    # MYSQL_PWD = os.getenv('MYSQL_PWD')
 
-    g.db = mysql.connector.connect(
-        host='localhost',
-        user='root',
-        password=MYSQL_PWD,
-        database='spotify_toolkit'
-    )
-    g.cursor = g.db.cursor()
+    # g.db = mysql.connector.connect(
+    #     host='localhost',
+    #     user='root',
+    #     password=MYSQL_PWD,
+    #     database='spotify_toolkit'
+    # )
+    # g.cursor = g.db.cursor()
 
 @bp.route('/<int:track_id>', methods=['GET'])
 def get_by_id(track_id: int):
-    g.cursor.execute('SELECT * FROM tracks WHERE id = %s', [track_id])
+    # g.cursor.execute('SELECT * FROM tracks WHERE id = %s', [track_id])
 
-    values = g.cursor.fetchall()[0]
-    return values[1]
+    # values = g.cursor.fetchall()[0]
+    values = [0, 1]
+    return jsonify(v=values)
 
 @bp.route('', methods=['GET'])
 def get_all():
-    g.cursor.execute('SELECT * FROM tracks')
-
-    values = g.cursor.fetchall()
+    # g.cursor.execute('SELECT * FROM tracks')
+    values = [0, 1]
+    # values = g.cursor.fetchall()
     return jsonify(v=values)
