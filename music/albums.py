706ad25189b09b056680b7984dd17614d6f25b86
benfry128 <benfry128@gmail.com>
2025-05-20 22:17:08 -0400

    Replace local mysql db info with AWS RDS info

--- music/albums.py
+++ music/albums.py
@@ -1,21 +1,27 @@
 from flask import Blueprint, g, jsonify
 import mysql.connector
-import os
+from globals import MYSQL_PWD
 
 bp = Blueprint('albums', __name__, url_prefix='/albums')
 
 @bp.before_request
 def load_db():
-    MYSQL_PWD = os.getenv('MYSQL_PWD')
-
     g.db = mysql.connector.connect(
-        host='localhost',
-        user='root',
+        host='albums.c4hk40ksa2ki.us-east-1.rds.amazonaws.com',
+        user='admin',
         password=MYSQL_PWD,
         database='albums'
     )
     g.cursor = g.db.cursor()
     print("HI")
+
+
+@bp.after_request
+def close_db_connections(response):
+    g.db.close()
+    g.cursor.close()
+    return response
+
 
 @bp.route('/queue', methods=['GET'])
 def get_by_id():
