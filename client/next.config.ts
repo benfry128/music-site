042bd2ea6f6cd3eb1717db726ad95b0bf43d5cb7
benfry128 <benfry128@gmail.com>
2025-05-22 00:06:22 -0400

    Remove old API config info

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,15 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  rewrites: async () => {
-    return [
-      {
-        source: '/api/:path*',
-        destination:
-        process.env.NODE_ENV === 'development' ? 'http://127.0.0.1:5000/:path*' : '/api/',
-      }
-    ]
-  },
   reactStrictMode: true,
 };
 
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,21 +1,12 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Typography, Box, Stack } from '@mui/material';
 
 function Index() {
-  useEffect(() => {
-    // fetch('http://localhost:8080/tracks/200')
-    //   .then(
-    //   data => {
-    //     console.log(data.json());
-    //   }
-    // );
-  }, []);
-  
   return (
     <Box sx={{ width: '100%', display: 'flex', flexDirection: 'row', justifyContent: 'center'}}>
       <Stack direction="column" alignItems='center' spacing={3}>
         <Typography variant='h3'>
-          Welcome to my Wobsite
+          Welcome to my Website
         </Typography>
         <Typography maxWidth='50%'>
           This is the homepage. There is nothing here for now. If you came here from my Instagram account, you may be interested in the Request Albums page or the listening data.
