c8a3db2476f550698c4125d40a9075871e8d2caf
benfry128 <benfry128@gmail.com>
2025-05-26 23:45:01 -0400

    Remove aws-amplify-backend since it's cooked We'll store secrets another way

--- frontend/components/Globals.tsx
+++ frontend/components/Globals.tsx
@@ -1,4 +1,1 @@
-import { secret } from '@aws-amplify/backend';
-
 export const API_URL = process.env.NODE_ENV === 'development' ? 'http://127.0.0.1:5000' : 'https://api.benfrymusic.com';
-export const ADMIN_PW = process.env.NODE_ENV === 'development' ? process.env.NEXT_PUBLIC_ADMIN_PW : secret('ADMIN_PW');
--- frontend/pages/admin.tsx
+++ frontend/pages/admin.tsx
@@ -3,7 +3,6 @@ import { Typography, Box, Stack, TextField } from '@mui/material';
 
 function Index() {
     const [_password, setPassword] = useState('');
-    console.log(process.env.NEXT_PUBLIC_ADMIN_PW);
 
     return <Box sx={{ width: '100%', display: 'flex', flexDirection: 'row', justifyContent: 'center'}}>
         <Stack direction="column" alignItems='center' spacing={3}>
