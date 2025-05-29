4d9a6c9fca30cc3aac0496e440a56a53c2e4d292
benfry128 <benfry128@gmail.com>
2025-05-28 22:18:35 -0400

    Completed album info table

--- frontend/pages/admin.tsx
+++ frontend/pages/admin.tsx
@@ -12,19 +12,36 @@ import TableRow from '@mui/material/TableRow';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
 
+interface Album {
+    id: number; 
+    title: string;
+    artist: string;
+    date_released: number;
+    rating: number;
+    date_listened: number;
+    favorite_song: string;
+    recommended_by: string;
+    ranking: number; 
+    queue_position: number
+}
+
 function Index() {
     const [_password, setPassword] = useState('');
-    const [_albums, setAlbums] = useState([]);
+    const [_albums, setAlbums] = useState<Album[]>([]);
 
     const getAlbums = async () => {
         const response = await fetch(`${API_URL}/albums/`);
-        const nums = await response.json();
-        setAlbums(nums)
+        const nums : { albums: Album[]} = await response.json();
+        setAlbums(nums.albums);
     }
 
     useEffect( () => {
         getAlbums();
     }, [])
+
+    if (_albums[0]) {
+        console.log(_albums[0].date_released);
+    }
 
     return <Box sx={{ width: '100%', display: 'flex', flexDirection: 'row', justifyContent: 'center'}}>
         <Stack direction="column" alignItems='center' spacing={3}>
@@ -44,11 +61,29 @@ function Index() {
                         <TableHead>
                             <TableRow>
                                 <TableCell>ID</TableCell>
+                                <TableCell>Title</TableCell>
+                                <TableCell>Artist</TableCell>
+                                <TableCell>Released</TableCell>
+                                <TableCell>Rating</TableCell>
+                                <TableCell>Listened</TableCell>
+                                <TableCell>Top Song</TableCell>
+                                <TableCell>Recommended By</TableCell>
+                                <TableCell>Ranking</TableCell>
+                                <TableCell>Queue Position</TableCell>
                             </TableRow>
                         </TableHead>
                         <TableBody>
-                            {_albums.map((album: { id: number, image_url: string, title: string, artist: string }) => <TableRow key={album.id}>
+                            {_albums.map((album: Album) => <TableRow key={album.id}>
                                 <TableCell>{album.id}</TableCell>
+                                <TableCell>{album.title}</TableCell>
+                                <TableCell>{album.artist}</TableCell>
+                                <TableCell>{album.date_released?.toString()}</TableCell>
+                                <TableCell>{album.rating}</TableCell>
+                                <TableCell>{album.date_listened?.toString()}</TableCell>
+                                <TableCell>{album.favorite_song}</TableCell>
+                                <TableCell>{album.recommended_by}</TableCell>
+                                <TableCell>{album.ranking}</TableCell>
+                                <TableCell>{album.queue_position}</TableCell>
                             </TableRow>)}
                         </TableBody>
                     </Table>
