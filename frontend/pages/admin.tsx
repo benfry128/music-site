ed8656e61570421f351c7c9298a45a00b71fa9c2
benfry128 <benfry128@gmail.com>
2025-05-28 21:44:18 -0400

    Added first stab at table of albums to admin page

--- frontend/pages/admin.tsx
+++ frontend/pages/admin.tsx
@@ -1,8 +1,30 @@
-import { useState } from 'react';
-import { Typography, Box, Stack, TextField } from '@mui/material';
+import { useEffect, useState } from 'react';
+import { API_URL } from '@/components/Globals';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Stack from '@mui/material/Stack';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
 
 function Index() {
     const [_password, setPassword] = useState('');
+    const [_albums, setAlbums] = useState([]);
+
+    const getAlbums = async () => {
+        const response = await fetch(`${API_URL}/albums/`);
+        const nums = await response.json();
+        setAlbums(nums)
+    }
+
+    useEffect( () => {
+        getAlbums();
+    }, [])
 
     return <Box sx={{ width: '100%', display: 'flex', flexDirection: 'row', justifyContent: 'center'}}>
         <Stack direction="column" alignItems='center' spacing={3}>
@@ -16,10 +38,21 @@ function Index() {
                     setPassword(e.target.value);
                 }}
             />
-            {_password === process.env.NEXT_PUBLIC_ADMIN_PW &&
-                <Typography>
-                    Nice
-                </Typography>
+            {_password === 'amorgos' &&
+                <TableContainer component={Paper}>
+                    <Table>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>ID</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {_albums.map((album: { id: number, image_url: string, title: string, artist: string }) => <TableRow key={album.id}>
+                                <TableCell>{album.id}</TableCell>
+                            </TableRow>)}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
             }
         </Stack>
     </Box>;
