891b4e7da536cf6525ce08eb726d9305d0483d82
benfry128 <benfry128@gmail.com>
2025-05-30 23:03:03 -0400

    Upgrade album table for sorting, etc.

--- frontend/pages/admin.tsx
+++ frontend/pages/admin.tsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from 'react';
 import { API_URL } from '@/components/Globals';
 import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
 import Stack from '@mui/material/Stack';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
@@ -9,6 +8,7 @@ import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
+import TableSortLabel from '@mui/material/TableSortLabel';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
 
@@ -25,9 +25,50 @@ interface Album {
     queue_position: number
 }
 
+// from https://mui.com/material-ui/react-table/
+function descendingComparator<T>(a: T, b: T, orderBy: keyof T) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
+}
+
+interface HeadCell {
+  id: keyof Album;
+  label: string;
+}
+
+const headCells: readonly HeadCell[] = [
+  {
+    id: 'title',
+    label: 'Title',
+  },
+  {
+    id: 'artist',
+    label: 'Artist',
+  },
+  {
+    id: 'date_released',
+    label: 'Released',
+  },
+  {
+    id: 'rating',
+    label: 'Rating',
+  },
+  {
+    id: 'date_listened',
+    label: 'Listened',
+  },
+];
+
 function Index() {
     const [_password, setPassword] = useState('');
     const [_albums, setAlbums] = useState<Album[]>([]);
+    const [order, setOrder] = useState<'asc' | 'desc'>('asc');
+    const [orderBy, setOrderBy] = useState<keyof Album>('title');
 
     const getAlbums = async () => {
         const response = await fetch(`${API_URL}/albums/`);
@@ -39,55 +80,62 @@ function Index() {
         getAlbums();
     }, [])
 
-    if (_albums[0]) {
-        console.log(_albums[0].date_released);
-    }
-
     return <Box sx={{ width: '100%', display: 'flex', flexDirection: 'row', justifyContent: 'center'}}>
         <Stack direction="column" alignItems='center' spacing={3}>
             <Typography variant='h3'>
                 Admin
             </Typography>
-            <TextField
-                label='Enter password'
-                value={_password}
-                onChange={(e) => {
-                    setPassword(e.target.value);
-                }}
-            />
-            {_password === 'amorgos' &&
-                <TableContainer component={Paper}>
-                    <Table size='small'>
+            {_password !== 'amorgos' ?
+                <TextField
+                    label='Enter password'
+                    value={_password}
+                    onChange={(e) => {
+                        setPassword(e.target.value);
+                    }}
+                />
+                : <TableContainer>
+                    <Table size={'small'}>
                         <TableHead>
                             <TableRow>
-                                <TableCell>ID</TableCell>
-                                <TableCell>Title</TableCell>
-                                <TableCell>Artist</TableCell>
-                                <TableCell>Released</TableCell>
-                                <TableCell>Rating</TableCell>
-                                <TableCell>Listened</TableCell>
-                                <TableCell>Top Song</TableCell>
-                                <TableCell>Recommended By</TableCell>
-                                <TableCell>Ranking</TableCell>
-                                <TableCell>Queue Position</TableCell>
+                                {headCells.map((headCell) => <TableCell key={headCell.id} >
+                                    <TableSortLabel
+                                        active={orderBy === headCell.id}
+                                        direction={orderBy === headCell.id ? order : 'asc'}
+                                        onClick={() => {
+                                            const isAsc = orderBy === headCell.id && order === 'asc';
+                                            setOrder(isAsc ? 'desc' : 'asc');
+                                            setOrderBy(headCell.id);
+                                        }}
+                                    >
+                                        {headCell.label}
+                                    </TableSortLabel>
+                                </TableCell>
+                                )}
                             </TableRow>
                         </TableHead>
                         <TableBody>
-                            {_albums.map((album: Album) => <TableRow key={album.id}>
-                                <TableCell>{album.id}</TableCell>
-                                <TableCell>{album.title}</TableCell>
-                                <TableCell>{album.artist}</TableCell>
-                                <TableCell>{album.date_released?.toString()}</TableCell>
-                                <TableCell>{album.rating}</TableCell>
-                                <TableCell>{album.date_listened?.toString()}</TableCell>
-                                <TableCell>{album.favorite_song}</TableCell>
-                                <TableCell>{album.recommended_by}</TableCell>
-                                <TableCell>{album.ranking}</TableCell>
-                                <TableCell>{album.queue_position}</TableCell>
-                            </TableRow>)}
+                            {_albums.sort(order === 'desc' ? (a, b) => descendingComparator(a, b, orderBy) : (a, b) => -descendingComparator(a, b, orderBy)).map((row) => 
+                                <TableRow key={row.id}>
+                                    <TableCell>{row.title}</TableCell>
+                                    <TableCell>{row.artist}</TableCell>
+                                    <TableCell>{row.date_released}</TableCell>
+                                    <TableCell align='center'>{row.rating}</TableCell>
+                                    <TableCell>{row.date_listened}</TableCell>
+                                </TableRow>
+                            )}
                         </TableBody>
                     </Table>
                 </TableContainer>
+                //                 <TableCell>ID</TableCell>
+                //                 <TableCell>Title</TableCell>
+                //                 <TableCell>Artist</TableCell>
+                //                 <TableCell>Released</TableCell>
+                //                 <TableCell>Rating</TableCell>
+                //                 <TableCell>Listened</TableCell>
+                //                 <TableCell>Top Song</TableCell>
+                //                 <TableCell>Recommended By</TableCell>
+                //                 <TableCell>Ranking</TableCell>
+                //                 <TableCell>Queue Position</TableCell>
             }
         </Stack>
     </Box>;
