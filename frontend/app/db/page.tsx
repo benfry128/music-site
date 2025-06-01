7e23ae4fbe3c0e7059523a1fca11acaefecb655f
benfry128 <benfry128@gmail.com>
2025-05-31 23:01:22 -0400

    Move db stuff directly to page

--- frontend/app/db/page.tsx
+++ frontend/app/db/page.tsx
@@ -1,5 +1,12 @@
-import DB from '../db'
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
 
 export default async function Page() {
-    return <DB />;
+    return <Box sx={{ width: '100%', display: 'flex', flexDirection: 'row', justifyContent: 'center'}}>
+		<Stack direction="column" alignItems='center' spacing={3}>
+			<Typography variant='h3'>DB</Typography>
+			<Typography maxWidth='50%'>Here you can check out the stuff that I&apos;ve listened to since I started recording it all in January 2024.</Typography>
+		</Stack>
+	</Box>;
 }
