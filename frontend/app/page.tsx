f2d62cc336d30184c06619274efb49398f53154d
benfry128 <benfry128@gmail.com>
2025-05-31 23:03:18 -0400

    Move home page to its own server side component

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,5 +1,14 @@
-import HomePage from './home-page'
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
 
 export default async function Page() {
-    return <HomePage/>;
+    return <Box sx={{ width: '100%', display: 'flex', flexDirection: 'row', justifyContent: 'center'}}>
+		<Stack direction="column" alignItems='center' spacing={3}>
+			<Typography variant='h3'>Welcome to my Website</Typography>
+			<Typography maxWidth='50%'>
+				This is the homepage. There is nothing here for now. If you came here from my Instagram account, you may be interested in the Request Albums page or the listening data.
+			</Typography>
+		</Stack>
+	</Box>;
 }
