29e6312b9a0e2e72a7404b483e1b3126f49dd593
benfry128 <benfry128@gmail.com>
2025-06-08 20:58:36 -0400

    Add review album Dialog box

--- frontend/components/Admin.tsx
+++ frontend/components/Admin.tsx
@@ -36,6 +36,7 @@ const columns: GridColDef[] = [
 
 export default function Admin( {albums} : { albums: Album[] }) {
     const [_password, setPassword] = useState('');
+    const [_reviewDialogOpen, setReviewDialogOpen] = useState(false);
 
     return <Box sx={{ width: '100%', display: 'flex', flexDirection: 'row', justifyContent: 'center'}}>
         <Stack direction="column" alignItems='center' spacing={3}>
@@ -50,20 +51,104 @@ export default function Admin( {albums} : { albums: Album[] }) {
                         setPassword(e.target.value);
                     }}
                 />
-				: <DataGrid
-					rows={albums}
-					columns={columns}
-                    showToolbar
-                    processRowUpdate={ async (updatedRow: Album, originalRow: Album) => {
-                        const patchError = await patchAlbum(updatedRow);
-                        if (patchError) {
-                            return originalRow;
-                        }
-                        return updatedRow;
-                    }}
-				>
-				</DataGrid>
+				: <>
+                    <Button variant='outlined' onClick={() => setReviewDialogOpen(true)}>Review Album</Button>
+                    <DataGrid
+                        rows={albums}
+                        columns={columns}
+                        showToolbar
+                        processRowUpdate={ async (updatedRow: Album, originalRow: Album) => {
+                            const patchError = await patchAlbum(updatedRow);
+                            if (patchError) {
+                                return originalRow;
+                            }
+                            return updatedRow;
+                        }}
+                    >
+                    </DataGrid>
+                </>
             }
         </Stack>
+        <Dialog
+            onClose={() => setReviewDialogOpen(false)}
+            open={_reviewDialogOpen}
+            slotProps={{
+                paper: {
+                    component: 'form',
+                    onSubmit: (event: React.FormEvent<HTMLFormElement>) => {
+                        event.preventDefault();
+                        const formData = new FormData(event.currentTarget);
+                        const formJson = Object.fromEntries(formData.entries());
+                        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+                        const { album: _, ...patchObject} = { //remove album from object to send to patchAlbum
+                            ...formJson, 
+                            id: parseInt(formJson.album.toString().split(' ').slice(-1)[0]), 
+                            queue_position: null,
+                            album: null
+                        };
+                        // setReviewDialogOpen(false);
+                        patchAlbum(patchObject);
+                    }
+                }
+            }}
+            fullWidth
+            maxWidth='sm'
+        >
+            <DialogTitle>Review Album</DialogTitle>
+            <DialogContent>
+                <DialogContentText>
+                    Among Us
+                </DialogContentText>
+                <br/>
+                <Autocomplete
+                    options={albums.filter((album) => Number.isNaN(album.rating))}
+                    renderInput={(params) => <TextField {...params} label='Album' name='album' required/>}
+                    getOptionLabel={(album) => `${album.title} - ${album.artist} - ${album.id}`}
+                />
+                <br/>
+                <Box sx={{display: 'flex', flexDirection: 'row', justifyContent: 'center', gap: '10px'}}>
+                    <FormControl fullWidth required>
+                        <InputLabel>Rating</InputLabel>
+                        <Select
+                            name='rating'
+                            label='Rating'
+                            defaultValue=''
+                        >
+                            <MenuItem value=''>None</MenuItem>
+                            <MenuItem value={0}>0</MenuItem>
+                            <MenuItem value={1}>1</MenuItem>
+                            <MenuItem value={2}>2</MenuItem>
+                        </Select>
+                    </FormControl>
+                    <DatePicker 
+                        label='Date Listened' 
+                        defaultValue={dayjs()} 
+                        sx={{ minWidth: 200}} 
+                        name='date_listened'
+                        format='YYYY-MM-DD'
+                    />
+                    <TextField
+                        label='Recommended By'
+                        name='recommended_by'
+                        sx={{ minWidth: 200}}
+                        required
+                    >
+                        From
+                    </TextField>
+                </Box>
+                <br/>
+                <TextField
+                    label='Favorite Song'
+                    name='favorite_song'
+                    fullWidth
+                    required
+                >
+                    Favorite Track
+                </TextField>
+            </DialogContent>
+            <DialogActions>
+                <Button type='submit'>Submit Review</Button>
+            </DialogActions>
+        </Dialog>
     </Box>;
 }
