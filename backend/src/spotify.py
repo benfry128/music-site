134534f42a40744d7fc95670377b76dd3c1ca364
benfry128 <benfry128@gmail.com>
2025-06-10 01:51:47 -0400

    Add first spotify route

---
+++ backend/src/spotify.py
@@ -0,0 +1,17 @@
+from flask import Blueprint, g, jsonify, request
+from src.globals import spotipy_setup
+
+bp = Blueprint('spotify', __name__, url_prefix='/spotify')
+
+
+@bp.before_request
+def prep_spotipy():
+    g.sp = spotipy_setup()
+
+
+@bp.route('/search/<search_str>', methods=['GET'])
+def search_albums(search_str: str):
+    albums = g.sp.search(q=f'album:{search_str}', type='album', limit=10)
+    print(albums)
+
+    return 'hi'
