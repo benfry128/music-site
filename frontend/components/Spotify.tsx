80e499e62de85a42f82668caeafd612a83855675
benfry128 <benfry128@gmail.com>
2025-06-10 23:11:44 -0400

    Spotify autocomplete component getting there

--- frontend/components/Spotify.tsx
+++ frontend/components/Spotify.tsx
@@ -1,11 +1,48 @@
 'use client'
-import Button from '@mui/material/Button';
+
+import Autocomplete from '@mui/material/Autocomplete';
+import TextField from '@mui/material/TextField';
+
+import { useState } from 'react';
 import { searchSpotify } from '@/app/actions';
 
+interface SpAlbum {
+    id: string; 
+    name: string;
+    artists: {
+        name: string;
+    }[];
+}
+
+let timer : NodeJS.Timeout;
+
 export default function Spotify() {
-    return <Button
-        onClick={() => searchSpotify('kind of blue')}
-    >
-        Click Me
-    </Button>
+    const [_searchStr, setSearchStr] = useState('');
+    const [_possibleAlbums, setPossibleAlbums] = useState([]);
+
+    return <Autocomplete
+        inputValue={_searchStr}
+        onInputChange={ (event, newString) => {
+            setSearchStr(newString);
+
+            //throttling spotify requests
+            clearTimeout(timer);
+
+            if (newString) {
+                timer = setTimeout(async () => {
+                    const stuff = await searchSpotify(newString);
+                    setPossibleAlbums(stuff);
+                    console.log(stuff[0].artists[0].name);
+                }, 1000);
+            } else {
+                setPossibleAlbums([]);
+            }
+        }}
+        renderInput={(params) => <TextField {...params} label="Search for an album" />}
+        options={_possibleAlbums}
+        sx={{
+            minWidth: '300px'
+        }}
+        getOptionLabel={(album : SpAlbum) => `${album.name} - ${album.artists[0].name} - ${album.id}`}
+    />;
 }
