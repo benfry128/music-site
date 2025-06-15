bd823af03942cc7031cba8f04c40147521384c36
benfry128 <benfry128@gmail.com>
2025-06-15 17:01:57 -0400

    Dedupe albums from spotify get route

--- backend/src/spotify.py
+++ backend/src/spotify.py
@@ -13,4 +13,12 @@ def prep_spotipy():
 def search_albums(search_str: str):
     albums = g.sp.search(q=f'album:{search_str}', type='album', limit=10)['albums']['items']
 
-    return jsonify(albums=albums), 200
+    deduped_albums = []
+
+    for album in albums:
+        print(album['name'] + album['artists'][0]['name'])
+        print([(a['name'] + a['artists'][0]['name']) for a in deduped_albums])
+        if (album['name'] + album['artists'][0]['name']) not in [(a['name'] + a['artists'][0]['name']) for a in deduped_albums]:
+            deduped_albums.append(album)
+
+    return jsonify(albums=deduped_albums), 200
