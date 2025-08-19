8c489dcf66d44f4b2f74f907515ad445c6b33724
benfry128 <benfry128@gmail.com>
2025-08-19 00:54:34 -0400

    Swap out db and admin pages

--- frontend/app/admin/page.tsx
+++
@@ -1,9 +0,0 @@
-import Admin from '@/components/Admin';
-import { Album, API_URL, BackendAlbum, convertBackendAlbum } from '@/components/Globals';
-
-export default async function Page() {
-    const response = await fetch(`${API_URL}/albums`, { cache: 'no-store'} )
-    const albums : Album[] = (await response.json()).albums.map((album : BackendAlbum) => convertBackendAlbum(album))
-
-    return <Admin albums={albums}/>;
-}
--- frontend/app/db/page.tsx
+++ frontend/app/db/page.tsx
@@ -1,12 +1,9 @@
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import Stack from '@mui/material/Stack';
+import DB from '@/components/DB';
+import { Album, API_URL, BackendAlbum, convertBackendAlbum } from '@/components/Globals';
 
 export default async function Page() {
-    return <Box sx={{ width: '100%', display: 'flex', flexDirection: 'row', justifyContent: 'center'}}>
-		<Stack direction="column" alignItems='center' spacing={3}>
-			<Typography variant='h3'>DB</Typography>
-			<Typography maxWidth='50%'>Here you can check out the stuff that I&apos;ve listened to since I started recording it all in January 2024.</Typography>
-		</Stack>
-	</Box>;
+    const response = await fetch(`${API_URL}/albums`, { cache: 'no-store'} )
+    const albums : Album[] = (await response.json()).albums.map((album : BackendAlbum) => convertBackendAlbum(album))
+
+    return <DB albums={albums}/>;
 }
