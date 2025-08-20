dc6dd64d01610de6d24ac662ae875112cf427cda
benfry128 <benfry128@gmail.com>
2025-08-19 20:39:49 -0400

    Remove swiper styles from layout.tsx

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,9 +1,5 @@
 import Layout from "@/components/Layout";
 
-import 'swiper/css';
-import 'swiper/css/effect-coverflow';
-import './styles.css';
-
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return <html lang="en">
         <body>
