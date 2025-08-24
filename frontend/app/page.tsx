9dfa86329b7e43a6416010df11b4e02473544ffe
benfry128 <benfry128@gmail.com>
2025-08-24 18:55:14 -0400

    Padding for main title

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -4,7 +4,7 @@ import styles from './app.module.css';
 
 export default async function Page() {
     return <Stack direction="column" alignItems='center' spacing={3}>
-        <Typography maxWidth='90%' variant='h3' align='center'>Welcome to my Website</Typography>
+        <Typography maxWidth='90%' variant='h3' align='center' sx={{pt: 3}}>Welcome to my Website</Typography>
         <Typography align='center' className={styles.text}>This is the homepage. There is nothing here for now. Check out the menu above to see the rest of the page!</Typography>
     </Stack>;
 }
