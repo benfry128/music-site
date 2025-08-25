6cf297068d6cf3ec86cb9787843c08421660a058
benfry128 <benfry128@gmail.com>
2025-08-25 19:26:38 -0400

    Linting

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -6,10 +6,10 @@ const nextConfig: NextConfig = {
 
 module.exports = {
   images: {
-    remotePatterns: [
-      {hostname: 'music-site-images.s3.us-east-1.amazonaws.com'}, 
-      {hostname: 'i.scdn.co'}
-    ],
+      remotePatterns: [
+          {hostname: 'music-site-images.s3.us-east-1.amazonaws.com'},
+          {hostname: 'i.scdn.co'}
+      ],
   },
 }
 
